64955f10a54e56ba20a727e0a5e442e30cc78147
A-fethi <fethiabderrahmane1@gmail.com>
2024-02-02 02:50:32 -0800

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,6 +7,7 @@ def validUTF8(data):
     """Returns True if data is a valid UTF-8 encoding, else return False"""
     count = 0
     for num in data:
+        num = num & 0xFF  # Ensure only the least significant 8 bits are used
         if count == 0:
             if (num >> 5) == 0b110:
                 count = 1
@@ -14,7 +15,9 @@ def validUTF8(data):
                 count = 2
             elif (num >> 3) == 0b11110:
                 count = 3
-            elif (num >> 7):
+            elif (num >> 7) == 0:
+                continue
+            else:
                 return False
         else:
             if (num >> 6) != 0b10:
